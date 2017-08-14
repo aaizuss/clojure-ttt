ab6fa06e76f26c08c3dd854166f2c9ce5a99f776
aaizuss <aaizuss@gmail.com>
2017-08-14 12:34:45 -0700

    functions for creating and managing board state

--- src/clojure_ttt/core.clj
+++ src/clojure_ttt/core.clj
@@ -1,7 +1,21 @@
 (ns clojure-ttt.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn new-board []
+  (into {} (for [space (range 9) v [{:marked false, :mark nil}]] [space v])))
+
+(defn marked? [board space]
+  (get-in board [space :marked]))
+
+(defn mark-space [board space marker]
+  (assoc-in board [space] {:marked true :mark marker}))
+
+(defn remove-marker [board space]
+  "unmark the board at space (necessary for minimax algorithm)"
+  (assoc-in board [space] {:marked false :mark nil}))
+
+(defn valid-spaces [board]
+  (keep (fn [[space-index space-info]] (if-not (:marked space-info) space-index)) board))
+
+(defn valid-spaces-alt [board]
+  (for [[space-index space-info] board :when (not (:marked space-info))] space-index))
--- test/clojure_ttt/core_test.clj
+++ test/clojure_ttt/core_test.clj
@@ -2,6 +2,46 @@
   (:require [clojure.test :refer :all]
             [clojure-ttt.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest new-board-test
+  (testing "new board creates empty board with 9 spaces"
+    (is (= (new-board)
+      {0 {:marked false, :mark nil},
+       1 {:marked false, :mark nil},
+       2 {:marked false, :mark nil},
+       3 {:marked false, :mark nil},
+       4 {:marked false, :mark nil},
+       5 {:marked false, :mark nil},
+       6 {:marked false, :mark nil},
+       7 {:marked false, :mark nil},
+       8 {:marked false, :mark nil}}))))
+
+(def blank-board (new-board))
+(def marked-board
+  {0 {:marked true, :mark "x"},
+   1 {:marked false, :mark nil},
+   2 {:marked false, :mark nil},
+   3 {:marked false, :mark nil},
+   4 {:marked true, :mark "o"},
+   5 {:marked false, :mark nil},
+   6 {:marked false, :mark nil},
+   7 {:marked false, :mark nil},
+   8 {:marked true, :mark "x"}})
+
+(deftest marked-test
+  (testing "returns false when space is empty"
+    (is (= false (marked? blank-board 4))))
+  (testing "returns true when space is marked"
+    (is (= true (marked? marked-board 0)))))
+
+(deftest mark-space-test
+  (testing "marks space by changing marked to true and filling in the mark"
+    (is (= {:marked true, :mark "x"}
+          (let [board (mark-space blank-board 1 "x")
+                properties (get board 1)]
+        properties)))))
+
+(deftest valid-spaces-test
+  (testing "all spaces are valid on a new board"
+    (is (= (set (range 9)) (set (valid-spaces blank-board))))
+  (testing "marked spaces are not included in list"
+    (is (= (set '(1 2 3 5 6 7)) (set (valid-spaces marked-board)))))))
