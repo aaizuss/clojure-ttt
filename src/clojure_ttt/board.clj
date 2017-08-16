2315757765dd3803b60a712b82f313ab2f2955a9
aaizuss <aaizuss@gmail.com>
2017-08-16 09:39:31 -0700

    comment out old board tests and functions. new board creation and mark functions work

--- src/clojure_ttt/board.clj
+++ src/clojure_ttt/board.clj
@@ -1,118 +1,120 @@
 (ns clojure-ttt.board
   (:gen-class))
 
+(def empty-space :_)
+
 (defn new-board []
-  (into (sorted-map) (into {} (for [space (range 9) value [{:marked false, :mark nil}]] [space value]))))
+  (into (sorted-map) (for [space (range 9) value [empty-space]] [space value])))
 
 (defn marked? [board space]
-  (get-in board [space :marked]))
+  (not (= (get board space) empty-space)))
 
 (defn mark-space [board space marker]
-  (assoc-in board [space] {:marked true :mark marker}))
+  (assoc board space marker))
 
-(defn empty-spaces [board]
-  (keep (fn [[space-index space-info]] (if-not (:marked space-info) space-index)) board))
-
-(defn empty-spaces-alt [board]
-  (for [[space-index space-info] board :when (not (:marked space-info))] space-index))
-
-(defn full? [board]
-  (= 0 (count (empty-spaces board))))
-
-(defn rows [board]
-  (mapv vec (partition 3 board)))
-
-(defn columns [board]
-  (apply mapv vector (rows board)))
-
-(defn diagonal-top-left [board]
-  (vec (take 3 (take-nth 4 board))))
-
-(defn diagonal-top-right [board]
-  (vec (select-keys board [2 4 6])))
-
-(defn diagonals [board]
-  [(diagonal-top-left board) (diagonal-top-right board)])
-
-(defn- consecutive-marks? [row]
-  (let [mark-status (map :marked (vals row))]
-    (every? true? mark-status)))
-
-(defn winner-on-row? [board row-index]
-  (let [row (apply assoc {} (interleave (flatten ((rows board) row-index))))]
-    (if (consecutive-marks? row) (apply = (map :mark (vals row))) false)))
-
-(defn row-winner? [board]
-  (loop [row-index 0]
-    (cond
-      (> row-index 2) false ; try not to rely on literals!
-      (winner-on-row? board row-index) true
-      :else (recur (inc row-index)))))
-
-(defn winner-on-column? [board col-index]
-  (let [column (apply assoc {} (interleave (flatten ((columns board) col-index))))]
-    (if (consecutive-marks? column) (apply = (map :mark (vals column))) false)))
-
-(defn column-winner? [board]
-  (loop [col-index 0]
-    (cond
-      (> col-index 2) false
-      (winner-on-column? board col-index) true
-      :else (recur (inc col-index)))))
-
-(defn winner-on-diag? [board diag-index]
-  (let [diag (apply assoc {} (interleave (flatten ((diagonals board) diag-index))))]
-    (if (consecutive-marks? diag) (apply = (map :mark (vals diag))) false)))
-
-(defn diag-winner? [board]
-  (loop [diag-index 0]
-    (cond
-      (> diag-index 1) false ; need to figure out a better way...
-      (winner-on-diag? board diag-index) true
-      :else (recur (inc diag-index)))))
-
-(defn has-winner? [board]
-  (or (row-winner? board) (column-winner? board) (diag-winner? board)))
-
-(defn tie? [board]
-  (and (full? board) (not (has-winner? board))))
-
-;;;;;;;;;;; printing - will move to a different namespace ;;;;;;;;;;;
-;; note to tom: i've just been testing thee in the repl - i have a lot to figure out
-
-(defn get-row [board row-index]
-  (apply assoc {} (interleave (flatten ((rows board) row-index)))))
-
-; {6 {:marked false, :mark nil}, 7 {:marked false, :mark nil}, 8 {:marked true, :mark "o"}} print this!
-; for key in map, if :marked is false, add key to string. else add :mark.
-; then interpose the string with " | "
-; (for [[space-index space-info] board :when (not (:marked space-info))] space-index))
-
-; map this over the row
-(defn render-space [row position] ; change row to board?
-  (let [info (row position)
-        marked (:marked info)]
-    (if marked (:mark info) (str position))))
-
-(defn indices-for-row [row-index]
-  (get (vec (map vec (partition 3 (range 9))))) row-index)
-
-
-(defn row-divider []
-  (let [part (str (apply str (take 3 (repeat "-"))) " ")
-        parts (take 3 (repeat part))]
-      (apply str parts)))
-
-
-;  0 | 1 | 2
-; ---*---*---
-;  3 | 4 | 5
-; ---*---*---
-;  6 | 7 | 8
+; (defn empty-spaces [board]
+;   (keep (fn [[space-index space-info]] (if-not (:marked space-info) space-index)) board))
 ;
-;  0 | 1 | 2
-; --- --- ---
-;  3 | 4 | 5
-; --- --- ---
-;  6 | 7 | 8
+; (defn empty-spaces-alt [board]
+;   (for [[space-index space-info] board :when (not (:marked space-info))] space-index))
 ;
+; (defn full? [board]
+;   (= 0 (count (empty-spaces board))))
+;
+; (defn rows [board]
+;   (mapv vec (partition 3 board)))
+;
+; (defn columns [board]
+;   (apply mapv vector (rows board)))
+;
+; (defn diagonal-top-left [board]
+;   (vec (take 3 (take-nth 4 board))))
+;
+; (defn diagonal-top-right [board]
+;   (vec (select-keys board [2 4 6])))
+;
+; (defn diagonals [board]
+;   [(diagonal-top-left board) (diagonal-top-right board)])
+;
+; (defn- consecutive-marks? [row]
+;   (let [mark-status (map :marked (vals row))]
+;     (every? true? mark-status)))
+;
+; (defn winner-on-row? [board row-index]
+;   (let [row (apply assoc {} (interleave (flatten ((rows board) row-index))))]
+;     (if (consecutive-marks? row) (apply = (map :mark (vals row))) false)))
+;
+; (defn row-winner? [board]
+;   (loop [row-index 0]
+;     (cond
+;       (> row-index 2) false ; try not to rely on literals!
+;       (winner-on-row? board row-index) true
+;       :else (recur (inc row-index)))))
+;
+; (defn winner-on-column? [board col-index]
+;   (let [column (apply assoc {} (interleave (flatten ((columns board) col-index))))]
+;     (if (consecutive-marks? column) (apply = (map :mark (vals column))) false)))
+;
+; (defn column-winner? [board]
+;   (loop [col-index 0]
+;     (cond
+;       (> col-index 2) false
+;       (winner-on-column? board col-index) true
+;       :else (recur (inc col-index)))))
+;
+; (defn winner-on-diag? [board diag-index]
+;   (let [diag (apply assoc {} (interleave (flatten ((diagonals board) diag-index))))]
+;     (if (consecutive-marks? diag) (apply = (map :mark (vals diag))) false)))
+;
+; (defn diag-winner? [board]
+;   (loop [diag-index 0]
+;     (cond
+;       (> diag-index 1) false ; need to figure out a better way...
+;       (winner-on-diag? board diag-index) true
+;       :else (recur (inc diag-index)))))
+;
+; (defn has-winner? [board]
+;   (or (row-winner? board) (column-winner? board) (diag-winner? board)))
+;
+; (defn tie? [board]
+;   (and (full? board) (not (has-winner? board))))
+;
+; ;;;;;;;;;;; printing - will move to a different namespace ;;;;;;;;;;;
+; ;; note to tom: i've just been testing thee in the repl - i have a lot to figure out
+;
+; (defn get-row [board row-index]
+;   (apply assoc {} (interleave (flatten ((rows board) row-index)))))
+;
+; ; {6 {:marked false, :mark nil}, 7 {:marked false, :mark nil}, 8 {:marked true, :mark "o"}} print this!
+; ; for key in map, if :marked is false, add key to string. else add :mark.
+; ; then interpose the string with " | "
+; ; (for [[space-index space-info] board :when (not (:marked space-info))] space-index))
+;
+; ; map this over the row
+; (defn render-space [row position] ; change row to board?
+;   (let [info (row position)
+;         marked (:marked info)]
+;     (if marked (:mark info) (str position))))
+;
+; (defn indices-for-row [row-index]
+;   (get (vec (map vec (partition 3 (range 9))))) row-index)
+;
+;
+; (defn row-divider []
+;   (let [part (str (apply str (take 3 (repeat "-"))) " ")
+;         parts (take 3 (repeat part))]
+;       (apply str parts)))
+;
+;
+; ;  0 | 1 | 2
+; ; ---*---*---
+; ;  3 | 4 | 5
+; ; ---*---*---
+; ;  6 | 7 | 8
+; ;
+; ;  0 | 1 | 2
+; ; --- --- ---
+; ;  3 | 4 | 5
+; ; --- --- ---
+; ;  6 | 7 | 8
+; ;
--- test/clojure_ttt/board_test.clj
+++ test/clojure_ttt/board_test.clj
@@ -5,93 +5,50 @@
 (deftest new-board-test
   (testing "new board creates empty board with 9 spaces"
     (is (= (new-board)
-      {0 {:marked false, :mark nil},
-       1 {:marked false, :mark nil},
-       2 {:marked false, :mark nil},
-       3 {:marked false, :mark nil},
-       4 {:marked false, :mark nil},
-       5 {:marked false, :mark nil},
-       6 {:marked false, :mark nil},
-       7 {:marked false, :mark nil},
-       8 {:marked false, :mark nil}}))))
+      {0 :_, 1 :_, 2 :_,
+       3 :_, 4 :_, 5 :_,
+       6 :_, 7 :_, 8 :_}))))
 
 (def blank-board (new-board))
 
-; note: important that these are sorted maps
 (def marked-board
   (into (sorted-map)
-    {0 {:marked true, :mark "x"},
-     1 {:marked false, :mark nil},
-     2 {:marked false, :mark nil},
-     3 {:marked false, :mark nil},
-     4 {:marked true, :mark "o"},
-     5 {:marked false, :mark nil},
-     6 {:marked false, :mark nil},
-     7 {:marked false, :mark nil},
-     8 {:marked true, :mark "x"}}))
+    {0 "x" 1 :_ 2 :_
+     3 :_ 4 "o" 5 :_
+     6 :_ 7 :_ 8 "x"}))
 
 (def row-0-winner
  (into (sorted-map)
-   {0 {:marked true, :mark "x"},
-    1 {:marked true, :mark "x"},
-    2 {:marked true, :mark "x"},
-    3 {:marked false, :mark nil},
-    4 {:marked true, :mark "o"},
-    5 {:marked false, :mark nil},
-    6 {:marked false, :mark nil},
-    7 {:marked false, :mark nil},
-    8 {:marked true, :mark "o"}}))
+   {0 "x" 1 "x" 2 "x"
+    3 :_ 4 :_ 5 :_
+    6 :_ 7 :_ 8 :_}))
 
 (def row-2-winner
  (into (sorted-map)
-   {0 {:marked false, :mark nil},
-    1 {:marked false, :mark nil},
-    2 {:marked true, :mark "x"},
-    3 {:marked false, :mark nil},
-    4 {:marked true, :mark nil},
-    5 {:marked true, :mark "x"},
-    6 {:marked true, :mark "o"},
-    7 {:marked true, :mark "o"},
-    8 {:marked true, :mark "o"}}))
+   {0 :_ 1 :_ 2 :_
+    3 "o" 4 "o" 5 "o"
+    6 :_ 7 :_ 8 :_}))
 
 (def col-2-winner
  (into (sorted-map)
-   {0 {:marked false, :mark nil},
-    1 {:marked false, :mark nil},
-    2 {:marked true, :mark "x"},
-    3 {:marked false, :mark nil},
-    4 {:marked false, :mark nil},
-    5 {:marked true, :mark "x"},
-    6 {:marked false, :mark nil},
-    7 {:marked true, :mark "o"},
-    8 {:marked true, :mark "x"}}))
+   {0 :_ 1 :_ 2 "o"
+    3 :_ 4 :_ 5 "o"
+    6 :_ 7 :_ 8 "o"}))
 
 (def diag-0-winner
   (into (sorted-map)
-    {0 {:marked true, :mark "o"},
-     1 {:marked true, :mark "x"},
-     2 {:marked true, :mark "x"},
-     3 {:marked false, :mark nil},
-     4 {:marked true, :mark "o"},
-     5 {:marked false, :mark nil},
-     6 {:marked false, :mark nil},
-     7 {:marked false, :mark nil},
-     8 {:marked true, :mark "o"}}))
+    {0 "x" 1 :_ 2 :_
+     3 :_ 4 "x" 5 :_
+     6 :_ 7 :_ 8 "x"}))
 
 (def tied-board
-  (into (sorted-map)
-    {0 {:marked true, :mark "x"},
-     1 {:marked true, :mark "x"},
-     2 {:marked true, :mark "o"},
-     3 {:marked true, :mark "o"},
-     4 {:marked true, :mark "o"},
-     5 {:marked true, :mark "x"},
-     6 {:marked true, :mark "x"},
-     7 {:marked true, :mark "o"},
-     8 {:marked true, :mark "x"}}))
+ (into (sorted-map)
+   {0 "x" 1 "x" 2 "o"
+    3 "o" 4 "o" 5 "x"
+    6 "x" 7 "o" 8 "x"}))
 
 (def full-board
-  (into {} (for [space (range 9) value [{:marked true, :mark "x"}]] [space value])))
+  (into (sorted-map) (for [space (range 9) value ["x"]] [space value])))
 
 (deftest marked-test
   (testing "returns false when space is empty"
@@ -100,115 +57,115 @@
     (is (= true (marked? marked-board 0)))))
 
 (deftest mark-space-test
-  (testing "marks space by changing marked to true and filling in the mark"
-    (is (= {:marked true, :mark "x"}
+  (testing "marks space 1 with x"
+    (is (= "x"
           (let [board (mark-space blank-board 1 "x")
-                properties (get board 1)]
-        properties)))))
+                mark (get board 1)]
+        mark)))))
 
-(deftest empty-spaces-test
-  (testing "all spaces are empty on a new board"
-    (is (= (set (range 9)) (set (empty-spaces blank-board))))
-  (testing "marked spaces are not included in list"
-    (is (= (set '(1 2 3 5 6 7)) (set (empty-spaces marked-board)))))))
-
-(deftest full-test
-  (testing "returns false for empty board"
-    (is (= false (full? blank-board))))
-  (testing "returns true for a full board"
-    (is (= true (full? full-board))))
-  (testing "returns false for partially marked board"
-    (is (= false (full? marked-board)))))
-
-(deftest rows-test
-  (testing "returns board partitioned as rows"
-    (is (=
-      [[[0 {:marked false, :mark nil}]
-        [1 {:marked false, :mark nil}]
-        [2 {:marked false, :mark nil}]]
-       [[3 {:marked false, :mark nil}]
-        [4 {:marked false, :mark nil}]
-        [5 {:marked false, :mark nil}]]
-       [[6 {:marked false, :mark nil}]
-        [7 {:marked false, :mark nil}]
-        [8 {:marked false, :mark nil}]]]
-          (rows blank-board)))))
-
-(deftest columns-test
-  (testing "returns board partitioned as columns"
-    (is (=
-      [[[0 {:marked false, :mark nil}]
-        [3 {:marked false, :mark nil}]
-        [6 {:marked false, :mark nil}]]
-       [[1 {:marked false, :mark nil}]
-        [4 {:marked false, :mark nil}]
-        [7 {:marked false, :mark nil}]]
-       [[2 {:marked false, :mark nil}]
-        [5 {:marked false, :mark nil}]
-        [8 {:marked false, :mark nil}]]]
-      (columns blank-board)))))
-
-(deftest diagonals-test
-  (testing "returns board partitioned as diagonals"
-    (is (=
-      [[[0 {:marked false, :mark nil}]
-        [4 {:marked false, :mark nil}]
-        [8 {:marked false, :mark nil}]]
-       [[2 {:marked false, :mark nil}]
-        [4 {:marked false, :mark nil}]
-        [6 {:marked false, :mark nil}]]]
-      (diagonals blank-board)))))
-
-(deftest winner-on-row-test
-  (testing "returns true when the given row has a winner"
-    (is (= true (winner-on-row? row-0-winner 0)))
-  (testing "returns false when the given row does not have a winner"
-    (is (= false (winner-on-row? row-0-winner 1))))))
-
-(deftest row-winner-test
-  (testing "returns true when the board has a row winner (0)"
-    (is (= true (row-winner? row-0-winner))))
-  (testing "returns true when the board has a row winner (2)"
-    (is (= true (row-winner? row-2-winner))))
-  (testing "returns false when the board does not have a row winner"
-    (is (= false (row-winner? col-2-winner)))))
-
-(deftest winner-on-column-test
-  (testing "returns true when the given column has a winner"
-    (is (= true (winner-on-column? col-2-winner 2))))
-  (testing "returns false when the given column does not have a winner"
-    (is (= false (winner-on-column? col-2-winner 0)))))
-
-(deftest column-winner-test
-  (testing "returns true when the board has a column winner"
-    (is (= true (column-winner? col-2-winner))))
-  (testing "returns false when the board does not have a column winner"
-    (is (= false (column-winner? row-0-winner))))
-  (testing "returns false when there is no column winner"
-    (is (= false (column-winner? blank-board)))))
-
-(deftest diag-winner-test
-  (testing "returns true when the board has a diagonal winner"
-    (is (= true (diag-winner? diag-0-winner))))
-  (testing "returns false when the board does not have a diagonal winner"
-    (is (= false (diag-winner? row-0-winner))))
-  (testing "returns false when there is no diagonal winner"
-    (is (= false (diag-winner? blank-board)))))
-
-(deftest has-winner-test?
-  (testing "returns true when the board has a row winner"
-    (is (= true (has-winner? row-2-winner))))
-  (testing "returns true when the board has a diagonal winner"
-    (is (= true (has-winner? diag-0-winner))))
-  (testing "returns true when the board has a column winner"
-    (is (= true (has-winner? col-2-winner))))
-  (testing "returns false when the board does not have a winner"
-    (is (= false (has-winner? marked-board)))))
-
-(deftest tie-test
-  (testing "true when there is a tie"
-    (is (= true (tie? tied-board))))
-  (testing "false when game is ongoing"
-    (is (= false (tie? marked-board))))
-  (testing "false when there is a winner"
-    (is (= false (tie? col-2-winner)))))
+; (deftest empty-spaces-test
+;   (testing "all spaces are empty on a new board"
+;     (is (= (set (range 9)) (set (empty-spaces blank-board))))
+;   (testing "marked spaces are not included in list"
+;     (is (= (set '(1 2 3 5 6 7)) (set (empty-spaces marked-board)))))))
+;
+; (deftest full-test
+;   (testing "returns false for empty board"
+;     (is (= false (full? blank-board))))
+;   (testing "returns true for a full board"
+;     (is (= true (full? full-board))))
+;   (testing "returns false for partially marked board"
+;     (is (= false (full? marked-board)))))
+;
+; (deftest rows-test
+;   (testing "returns board partitioned as rows"
+;     (is (=
+;       [[[0 {:marked false, :mark nil}]
+;         [1 {:marked false, :mark nil}]
+;         [2 {:marked false, :mark nil}]]
+;        [[3 {:marked false, :mark nil}]
+;         [4 {:marked false, :mark nil}]
+;         [5 {:marked false, :mark nil}]]
+;        [[6 {:marked false, :mark nil}]
+;         [7 {:marked false, :mark nil}]
+;         [8 {:marked false, :mark nil}]]]
+;           (rows blank-board)))))
+;
+; (deftest columns-test
+;   (testing "returns board partitioned as columns"
+;     (is (=
+;       [[[0 {:marked false, :mark nil}]
+;         [3 {:marked false, :mark nil}]
+;         [6 {:marked false, :mark nil}]]
+;        [[1 {:marked false, :mark nil}]
+;         [4 {:marked false, :mark nil}]
+;         [7 {:marked false, :mark nil}]]
+;        [[2 {:marked false, :mark nil}]
+;         [5 {:marked false, :mark nil}]
+;         [8 {:marked false, :mark nil}]]]
+;       (columns blank-board)))))
+;
+; (deftest diagonals-test
+;   (testing "returns board partitioned as diagonals"
+;     (is (=
+;       [[[0 {:marked false, :mark nil}]
+;         [4 {:marked false, :mark nil}]
+;         [8 {:marked false, :mark nil}]]
+;        [[2 {:marked false, :mark nil}]
+;         [4 {:marked false, :mark nil}]
+;         [6 {:marked false, :mark nil}]]]
+;       (diagonals blank-board)))))
+;
+; (deftest winner-on-row-test
+;   (testing "returns true when the given row has a winner"
+;     (is (= true (winner-on-row? row-0-winner 0)))
+;   (testing "returns false when the given row does not have a winner"
+;     (is (= false (winner-on-row? row-0-winner 1))))))
+;
+; (deftest row-winner-test
+;   (testing "returns true when the board has a row winner (0)"
+;     (is (= true (row-winner? row-0-winner))))
+;   (testing "returns true when the board has a row winner (2)"
+;     (is (= true (row-winner? row-2-winner))))
+;   (testing "returns false when the board does not have a row winner"
+;     (is (= false (row-winner? col-2-winner)))))
+;
+; (deftest winner-on-column-test
+;   (testing "returns true when the given column has a winner"
+;     (is (= true (winner-on-column? col-2-winner 2))))
+;   (testing "returns false when the given column does not have a winner"
+;     (is (= false (winner-on-column? col-2-winner 0)))))
+;
+; (deftest column-winner-test
+;   (testing "returns true when the board has a column winner"
+;     (is (= true (column-winner? col-2-winner))))
+;   (testing "returns false when the board does not have a column winner"
+;     (is (= false (column-winner? row-0-winner))))
+;   (testing "returns false when there is no column winner"
+;     (is (= false (column-winner? blank-board)))))
+;
+; (deftest diag-winner-test
+;   (testing "returns true when the board has a diagonal winner"
+;     (is (= true (diag-winner? diag-0-winner))))
+;   (testing "returns false when the board does not have a diagonal winner"
+;     (is (= false (diag-winner? row-0-winner))))
+;   (testing "returns false when there is no diagonal winner"
+;     (is (= false (diag-winner? blank-board)))))
+;
+; (deftest has-winner-test?
+;   (testing "returns true when the board has a row winner"
+;     (is (= true (has-winner? row-2-winner))))
+;   (testing "returns true when the board has a diagonal winner"
+;     (is (= true (has-winner? diag-0-winner))))
+;   (testing "returns true when the board has a column winner"
+;     (is (= true (has-winner? col-2-winner))))
+;   (testing "returns false when the board does not have a winner"
+;     (is (= false (has-winner? marked-board)))))
+;
+; (deftest tie-test
+;   (testing "true when there is a tie"
+;     (is (= true (tie? tied-board))))
+;   (testing "false when game is ongoing"
+;     (is (= false (tie? marked-board))))
+;   (testing "false when there is a winner"
+;     (is (= false (tie? col-2-winner)))))
