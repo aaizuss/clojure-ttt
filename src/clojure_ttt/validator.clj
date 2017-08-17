17798f4899f7fa7fdf56be253a7eede7bafd39c3
aaizuss <aaizuss@gmail.com>
2017-08-17 11:54:07 -0700

    detect if a move is valid

--- src/clojure_ttt/validator.clj
+++ src/clojure_ttt/validator.clj
@@ -1,2 +1,12 @@
 (ns clojure-ttt.validator
-  :require [clojure-ttt.board :as board])
+  (:require [clojure-ttt.board :as board]))
+
+(defn space-exists? [board space]
+  (and (>= space 0) (< space board/num-spaces)))
+
+; could just check that it's in board/empty-spaces...
+; but this is more efficient than getting all empty spaces...
+(defn valid-move? [board proposed-move]
+  (and
+    (space-exists? board proposed-move)
+    (= false (board/marked? board proposed-move))))
--- test/clojure_ttt/validator_test.clj
+++ test/clojure_ttt/validator_test.clj
@@ -2,3 +2,19 @@
   (:require [clojure.test :refer :all]
             [clojure-ttt.validator :refer :all]
             [clojure-ttt.board :as board]))
+
+(def blank-board (board/new-board))
+
+(def marked-board
+  (into (sorted-map)
+    {0 "x" 1 :_ 2 :_
+     3 :_ 4 "o" 5 :_
+     6 :_ 7 :_ 8 "x"}))
+
+(deftest valid-move-test
+  (testing "true if position is not marked"
+    (is (= true (valid-move? blank-board 2))))
+  (testing "false if the space does not exist"
+    (is (= false (valid-move? blank-board 9))))
+  (testing "false if the space is taken"
+    (is (= false (valid-move? marked-board 4)))))
