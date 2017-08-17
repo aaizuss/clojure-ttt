b1f23da2b18d33a5fe11b99fd7f662f30645218e
aaizuss <aaizuss@gmail.com>
2017-08-17 14:35:07 -0700

    view namespace

---
+++ src/clojure_ttt/view.clj
@@ -0,0 +1,3 @@
+(ns clojure-ttt.view
+  (:require [clojure-ttt.string-renderer :as renderer]
+            [clojure-ttt.io :as io]))
---
+++ test/clojure_ttt/view_test.clj
@@ -0,0 +1,3 @@
+(ns clojure-ttt.view-test
+  (:require [clojure.test :refer :all]
+            [clojure-ttt.view :refer :all]))
