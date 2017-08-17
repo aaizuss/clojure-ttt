76a994de92c769482e3e49989a6f1a69f3c60aa7
aaizuss <aaizuss@gmail.com>
2017-08-17 15:24:48 -0700

    get marker from user input

--- test/clojure_ttt/view_test.clj
+++ test/clojure_ttt/view_test.clj
@@ -1,3 +1,18 @@
 (ns clojure-ttt.view-test
   (:require [clojure.test :refer :all]
             [clojure-ttt.view :refer :all]))
+
+; i need to figure out how to prevent
+; the tests from printing
+(deftest get-marker-test
+  (testing "returns a valid mark"
+    (is (= "x"
+        (with-in-str "x" (get-marker :order-num 1)))))
+  (testing "continues asking for a marker until it is valid")
+    (is (= "x"
+        (with-in-str "?\n4\nbbb\nx"
+            (get-marker :order-num 1))))
+  (testing "continues asking for a marker if given opponent mark"
+    (is (= "x"
+        (with-in-str "a\nx"
+            (get-marker :order-num 2 :opponent-marker "a"))))))
