44a660084d399821252454c8aeaea7adf6daa57a
aaizuss <aaizuss@gmail.com>
2017-08-17 12:37:19 -0700

    add checks for parsing string as num and refactor functions so they each have a single purpose

--- src/clojure_ttt/validator.clj
+++ src/clojure_ttt/validator.clj
@@ -1,12 +1,26 @@
 (ns clojure-ttt.validator
-  (:require [clojure-ttt.board :as board]))
+  (:require [clojure-ttt.board :as board]
+            [clojure-ttt.utilities :as utils]))
 
+(defn is-num? [input]
+  (try
+    (Integer/parseInt input)
+    true
+  (catch Exception e false)))
+
+; should probably be private...
 (defn space-exists? [board space]
   (and (>= space 0) (< space board/num-spaces)))
 
+; might be better to put a variation of this in board ns
+(defn valid-board-position? [board space]
+  (and (is-num? space)
+       (space-exists? board (utils/input-to-num space))))
+
 ; could just check that it's in board/empty-spaces...
-; but this is more efficient than getting all empty spaces...
+; but using is-num and space-exists is more efficient than getting all empty spaces...
+; might want to refactor where i call utils/input-to-num
 (defn valid-move? [board proposed-move]
   (and
-    (space-exists? board proposed-move)
-    (= false (board/marked? board proposed-move))))
+    (valid-board-position? board proposed-move)
+    (= false (board/marked? board (utils/input-to-num proposed-move)))))
--- test/clojure_ttt/validator_test.clj
+++ test/clojure_ttt/validator_test.clj
@@ -11,10 +11,26 @@
      3 :_ 4 "o" 5 :_
      6 :_ 7 :_ 8 "x"}))
 
-(deftest valid-move-test
+(deftest is-num-test
+  (testing "false if not a number"
+    (is (= false (is-num? "a"))))
+  (testing "'5' can be parsed as a number"
+    (is (= true (is-num? "5")))))
+
+(deftest valid-board-position-test
+  (testing "false if not a number"
+    (is (= false (valid-board-position? blank-board "a"))))
+  (testing "false if not on board"
+    (is (= false (valid-board-position? blank-board "10"))))
+  (testing "true if position is on board"
+    (is (= true (valid-board-position? blank-board "1"))))
+  (testing "true if position is on board (even if it is marked)"
+    (is (= true (valid-board-position? marked-board "4")))))
+
+(deftest can-move-test
   (testing "true if position is not marked"
-    (is (= true (valid-move? blank-board 2))))
+    (is (= true (valid-move? blank-board "2"))))
   (testing "false if the space does not exist"
-    (is (= false (valid-move? blank-board 9))))
+    (is (= false (valid-move? blank-board "9"))))
   (testing "false if the space is taken"
-    (is (= false (valid-move? marked-board 4)))))
+    (is (= false (valid-move? marked-board "4")))))
