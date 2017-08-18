0c13649773f5c4cedeb1b2a8ad104dfb6ca03dec
aaizuss <aaizuss@gmail.com>
2017-08-18 10:53:49 -0700

    set up game ns

---
+++ src/clojure_ttt/game.clj
@@ -0,0 +1,4 @@
+(ns clojure-ttt.game
+  (:require [clojure-ttt.view :as view]
+            [clojure-ttt.board :as board]
+            [clojure-ttt.string-renderer as: render]))
---
+++ test/clojure_ttt/game_test.clj
@@ -0,0 +1,4 @@
+(ns clojure-ttt.game-test
+  (:require [clojure.test :refer :all]
+            [clojure-ttt.game :refer :all]
+            [clojure-ttt.board :as board]))
