bc6b2060a60e8f7965813a2fcaaf866b5ee1e3e0
aaizuss <aaizuss@gmail.com>
2017-08-19 21:18:16 -0700

    get player markers and show messages for win or tie

--- src/clojure_ttt/game.clj
+++ src/clojure_ttt/game.clj
@@ -4,22 +4,38 @@
             [clojure-ttt.io :as io]
             [clojure-ttt.string-renderer :as renderer]))
 
-(defn get-player-data []
+(defn setup-players []
   (let [p1-marker (view/get-marker)
-        p2-marker (view/get-marker 2 p1-marker)]
-      {:p1 p1-marker :p2 p2-marker}))
+        p2-marker (view/get-marker :order-num 2 :opponent-marker p1-marker)]
+  {:current-player p1-marker :opponent p2-marker}))
+
+(defn setup-game []
+  (let [players (setup-players)]
+  (assoc players :board (board/new-board))))
 
 (defn show-board-before-move [board current-player]
   (io/show (renderer/turn-message current-player))
   (io/show (renderer/render-board board)))
 
-(defn game-loop [& {:keys [board current-player opponent]
-                  :or {board (board/new-board) current-player "x" opponent "o"}}]
+(defn game-loop [{:keys [board current-player opponent]}]
   (show-board-before-move board current-player)
   (let [move (view/get-move board)
         marked-board (board/mark-space board move current-player)]
-    (if (or (board/has-winner? marked-board) (board/tie? marked-board))
-        (println "Game Over!")
-        (recur {:board marked-board :current-player opponent :opponent current-player}))))
+    (cond
+      (board/has-winner? marked-board)
+        (do
+          (io/show (renderer/render-board marked-board))
+          (io/show (renderer/win-message current-player)))
+      (board/tie? marked-board)
+        (do
+          (io/show (renderer/render-board marked-board))
+          (io/show (renderer/tie-message)))
+      :else
+          (recur
+            {:board marked-board
+             :current-player opponent
+             :opponent current-player}))))
 
-; show board when game is over
+(defn play []
+  (view/show-welcome)
+  (game-loop (setup-game)))
