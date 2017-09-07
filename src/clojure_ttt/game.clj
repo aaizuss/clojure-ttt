8f0976df5fd5ae2d0a6c653803e9de99860b549c
aaizuss <aaizuss@gmail.com>
2017-09-07 13:31:46 -0700

    only call db functions if connection succeeds

--- src/clojure_ttt/game.clj
+++ src/clojure_ttt/game.clj
@@ -92,7 +92,7 @@
   (if (= move undo)
       (undo-moves board move-history current-player opponent)
       (do
-        (game-db/update-db board current-player opponent move)
+        (game-db/update-db-if-up board current-player opponent move)
         (let [marked-board (board/mark-space board move marker)
               new-history (conj move-history move)]
           {:board marked-board
--- src/clojure_ttt/game_db.clj
+++ src/clojure_ttt/game_db.clj
@@ -34,12 +34,6 @@
         p1-board (string/replace specific-board (re-pattern p1-marker) (generic-marker player1))]
     (string/replace p1-board (re-pattern p2-marker) (generic-marker player2))))
 
-; to create a table called game
-(def game-sql (jdbc/create-table-ddl :game [[:state "VARCHAR(16) NOT NULL"]
-                                          [:turn "VARCHAR(1) NOT NULL"]
-                                          [:moves "integer[]"]
-                                          ["PRIMARY KEY" "(state, turn)"]]))
-
 (defn- upsert [board-state turn move]
   (let [quoted-turn (str "'" turn "', ")
         quoted-board (str "'" board-state "', ")
@@ -53,5 +47,8 @@
         turn (generic-marker player1)]
       (upsert board-state turn move)))
 
-; (def count-moves
-;   (jdbc/query db ["select state, move, count(*) as count from game, unnest(game.moves) as move group by 1,2 order by 1,3 desc"]))
+(defn update-db-if-up [board player1 player2 move]
+  (try
+    (update-db board player1 player2 move)
+    (catch org.postgresql.util.PSQLException _
+      (prn "db down!!"))))
